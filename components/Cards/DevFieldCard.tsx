75a7443aea825c225aab8066a958857551839f5b
Vishal.Karangale <Vishal.Karangale@ril.com>
2024-10-16 10:56:20 +0530

    Experience & Skills section bug fixes

--- components/Cards/DevFieldCard.tsx
+++ components/Cards/DevFieldCard.tsx
@@ -15,11 +15,11 @@ const DevFieldCard: React.FC<DevFieldCardProps> = ({ index, devFieldItem }) => {
   return (
     <div
       data-aos="fade-right"
-      data-aos-delay={500*index}
+      data-aos-delay={500 * index}
       data-aos-duration={150}
       data-aos-once="true"
     >
-      <Card className="bg-primary border-none px-0 pt-6 md:pt-12 w-[150px] md:w-[200px] h-44 md:h-52">
+      <Card className="bg-primary border-none px-0 pt-6 md:pt-12 w-[150px] md:w-[200px] h-44 md:h-52 hover:scale-110 transition-all cursor-pointer duration-200">
         <CardFooter className="flex px-2 md:px-6 flex-col justify-center items-start mt-8">
           {devFieldItem.icon}
           <p className="text-white font-poppins tracking-wide text-base md:text-xl mt-2 font-semibold">
